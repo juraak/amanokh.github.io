575f01e11613b8db2ddd88a40450000af53376bd
Anton Manokhin <a.manokh@gmail.com>
2018-08-04 15:35:40 +0800

    VK

--- 282/parser.js
+++ 282/parser.js
@@ -1,6 +1,6 @@
 
 function user_login(){
-    VK.Auth.login(user_registered, 8192);
+    VK.Auth.login(user_registered, 4);
 }
 function user_registered(session, status){
     alert(session['user']['id']);
