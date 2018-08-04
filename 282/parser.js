bde859a4fde3ee99cc523f3c5c498a1bec1c5dec
Anton Manokhin <a.manokh@gmail.com>
2018-08-04 15:30:11 +0800

    VK api test

--- 282/parser.js
+++ 282/parser.js
@@ -1,6 +1,7 @@
-VK.init({
-    apiId: 6651530
-  });
+
 function user_login(){
     VK.Auth.login(user_registered, 8192);
+}
+function user_registered(session, status){
+    alert(session['user']['id']);
 }
