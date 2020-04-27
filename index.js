a1f23e7aefd334dc0f4bb6ddc13aee954aab1dbd
Anton Manokhin <a.manokh@gmail.com>
2020-04-27 12:25:17 +0800

    Edit projects info

--- index.js
+++ index.js
@@ -3,6 +3,62 @@ const magicalUnderlines = Array.from(document.querySelectorAll('.underline--magi
 
 const gradientAPI = 'https://manokhin.live/gradient.json';
 
+const gradients = [
+    {
+      "name": "warmFlame",
+      "direction": "90deg",
+      "colors": ["#ff9a9e", "#fad0c4"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "youngPassion",
+      "direction": "90deg",
+      "colors": ["#ff8177", "#ff867a", "#ff8c7f", "#f99185", "#cf556c", "#b12a5b"],
+      "positions": ["0%", "0%", "21%", "52%", "78%", "100%"]
+    },
+    {
+      "name": "newLife",
+      "direction": "to right",
+      "colors": ["#43e97b", "#38f9d7"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "temptingAzure",
+      "direction": "120deg",
+      "colors": ["#84fab0", "#8fd3f4"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "ripeMalinka",
+      "direction": "120deg",
+      "colors": ["#f093fb", "#f5576c"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "trueSunset",
+      "direction": "to right",
+      "colors": ["#fa709a", "#fee140"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "plumPlate",
+      "direction": "135deg",
+      "colors": ["#667eea", "#764ba2"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "deepBlue",
+      "direction": "to right",
+      "colors": ["#6a11cb", "#2575fc"],
+      "positions": ["0%", "100%"]
+    },
+    {
+      "name": "sweetPeriod",
+      "direction": "to top",
+      "colors": ["#3f51b1", "#5a55ae", "#7b5fac", "#8f6aae", "#a86aa4", "#cc6b8e", "#f18271", "#f3a469", "#f7c978"],
+      "positions": ["0%", "13%", "25%", "38%", "50%", "62%", "75%", "87%", "100%"]
+    }
+  ];
 // HELPER FUNCTIONS
 
 // 1. Get random number in range. Used to get random index from array.
@@ -20,12 +76,7 @@ const addBackground = (elms) => (color) => {
     el.style.backgroundImage = color;
   });
 }
-// 4. Function to get data from API
-const getData = async(url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.data;
-}
+
 
 // 5. Partial Application of addBackground to always apply 
 // background to the magicalUnderlines constant
@@ -39,7 +90,7 @@ const buildGradient = (obj) => `linear-gradient(${obj.direction}, ${mergeArrays(
 // 2. Get single gradient from data pulled in array and
 // apply single gradient to a callback function
 const applyGradient = async(url, callback) => {
-  const data = await getData(url);
+  const data = gradients;
   const gradient = buildGradient(data[randNumInRange(data.length)]);
   callback(gradient);
 }
